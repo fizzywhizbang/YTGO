4913aaedf5039e1d95cf24f009a9c8b3a7c1cc18
FizzyWhizbang <marc.levine@gmail.com>
2022-02-24 07:40:14 -0600

    Update feedform.go

--- feedform.go
+++ feedform.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/aquilax/truncate"
@@ -29,7 +32,6 @@ func feedWindow(chanid string) {
 
 	mainWidget := widgets.NewQWidget(nil, 0)
 	mainWidget.SetContentsMargins(0, 2, 0, 0)
-	layout := widgets.NewQVBoxLayout()
 
 	youtubefeed := YtFeedURL + chanid
 
@@ -46,87 +48,110 @@ func feedWindow(chanid string) {
 	// we unmarshal our byteArray which contains our
 	// xmlFiles content into 'users' which we defined above
 	xml.Unmarshal(byteValue, &feed)
-	treeWidget := widgets.NewQTreeWidget(nil)
-	treeWidget.SetColumnCount(7)
-	treeWidget.SetObjectName("treewidget")
-	treeWidget.Header().SetStretchLastSection(false)
-	treeWidget.Header().SetSectionsClickable(true)
-	treeWidget.SetAlternatingRowColors(true)
-	tableColors := "alternate-background-color: #88DD88; background-color:#FFFFFF; color:#000000; font-size: 12px;"
-	treeWidget.SetStyleSheet(tableColors)
-	treeWidget.Header()
-	treeWidget.SetHeaderLabels([]string{"VidoeID", "Title", "Date", "Status", "View", "Mark", "Download"})
+
+	/*
+		create form widget (I believe the problem is sizing but if I can static set sizes it should be visually appealing)
+		insert elements into form widget
+		each form widget will have it's own connect.clicked with the value it needs to be
+
+	*/
+
+	formlayout := widgets.NewQFormLayout(nil)
+	group := widgets.NewQHBoxLayout()
+	header1 := widgets.NewQLabel2("VideoID", nil, 0)
+	header1.SetFixedWidth(100)
+	group.AddWidget(header1, 0, 0)
+	header2 := widgets.NewQLabel2("Title", nil, 0)
+	header2.SetFixedWidth(400)
+	group.AddWidget(header2, 0, 0)
+	header3 := widgets.NewQLabel2("Date", nil, 0)
+	header3.SetFixedWidth(100)
+	group.AddWidget(header3, 0, 0)
+	header4 := widgets.NewQLabel2("Status", nil, 0)
+	header4.SetFixedWidth(100)
+	group.AddWidget(header4, 0, 0)
+	header5 := widgets.NewQLabel2("Actions", nil, 0)
+	group.AddWidget(header5, 0, 0)
+
+	formlayout.AddRow6(group)
 
 	if len(feed.Entries) >= 1 {
 		//db fields yt_videoid, title, description, publisher, publish_date(unix), watched(if added to download then 1 else 0)
 		for i := 0; i < len(feed.Entries); i++ {
+			group := widgets.NewQHBoxLayout()
 
 			date, _ := time.Parse(time.RFC3339, feed.Entries[i].Published)
-			videxists := "False"
+			videxists := false
 			if database.GetVideoExist(config.Db_name, feed.Entries[i].VideoId) == 1 {
-				videxists = "True"
+				videxists = true
 			}
-			treewidgetItem := widgets.NewQTreeWidgetItem2([]string{feed.Entries[i].VideoId, truncate.Truncate(feed.Entries[i].Title, 70, "...", truncate.PositionEnd), date.Format("2006-01-02"), videxists, "View", "Mark", "Download"}, 0)
-			treewidgetItem.SetData(0, int(core.Qt__UserRole), core.NewQVariant12(feed.Entries[i].VideoId))
+			//fields VideoID, Title, Date, Downloaded, Action (view,mark,etc)
 
-			treeWidget.AddTopLevelItem(treewidgetItem)
+			videoIDLabel := widgets.NewQLabel2(feed.Entries[i].VideoId, nil, 0)
+			videoIDLabel.SetFixedWidth(100)
+			group.AddWidget(videoIDLabel, 0, 0)
+
+			titleLabel := widgets.NewQLabel2(truncate.Truncate(feed.Entries[i].Title, 70, "...", truncate.PositionEnd), nil, 0)
+			titleLabel.SetFixedWidth(400)
+			group.AddWidget(titleLabel, 0, 0)
+
+			dateLabel := widgets.NewQLabel2(date.Format("2006-01-02"), nil, 0)
+			dateLabel.SetFixedWidth(100)
+			group.AddWidget(dateLabel, 0, 0)
+
+			actionCombo := widgets.NewQComboBox(nil)
+			list := []string{"Actions", "Download " + strconv.Itoa(i) + "", "Skip " + strconv.Itoa(i) + "", "View " + strconv.Itoa(i) + ""}
+			actionCombo.AddItems(list)
+
+			status := "False"
+			if videxists {
+				videoData := database.GetVideoInfo(config.Db_name, feed.Entries[i].VideoId)
+				fmt.Println(videoData.ID, videoData.Downloaded)
+				status = "Queued"
+				if videoData.Downloaded == 1 {
+					status = "Downloaded"
+				}
+				if videoData.Downloaded == 2 {
+					status = "Skipped"
+				}
+
+			}
+
+			downloadedLabel := widgets.NewQLabel2(status, nil, 0)
+			downloadedLabel.SetFixedWidth(100)
+			group.AddWidget(downloadedLabel, 0, 0)
+
+			group.AddWidget(actionCombo, 0, 0)
+			actionCombo.ConnectCurrentTextChanged(func(text string) {
+				action := strings.Split(text, " ")
+
+				if action[0] == "Download" {
+					row, _ := strconv.Atoi(action[1])
+					fmt.Println("Download", feed.Entries[row].VideoId)
+					date, _ := time.Parse(time.RFC3339, feed.Entries[row].Published)
+					functions.MkCrawljob(config.Db_name, config.FolderWatch, GlobalChannelID, feed.Entries[row].Title, feed.Entries[row].VideoId, date.Format("2006-01-02"), 1)
+					widgets.QMessageBox_Information(nil, "OK", "Added to Queue "+feed.Entries[row].Title, widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
+				}
+				if action[0] == "Skip" {
+					row, _ := strconv.Atoi(action[1])
+					fmt.Println("Skip", feed.Entries[row].VideoId)
+					date, _ := time.Parse(time.RFC3339, feed.Entries[row].Published)
+					unixdate := functions.ConvertYMDtoUnix(date.Format("2006-01-02"))
+					database.InsertVideo(config.Db_name, feed.Entries[row].VideoId, feed.Entries[row].Title, feed.Entries[row].Title, GlobalChannelID, unixdate, "2")
+					widgets.QMessageBox_Information(nil, "OK", feed.Entries[row].Title+" Recorded as skipped", widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
+				}
+				if action[0] == "View" {
+					row, _ := strconv.Atoi(action[1])
+					url := YtWatchPrefix + feed.Entries[row].VideoId
+					functions.Openbrowser(url, config.Defbrowser)
+				}
+			})
+
+			formlayout.AddRow6(group)
 		}
 	}
-	database.UpdateFeedCT(config.Db_name, chanid, len(feed.Entries))
-	database.UpdateChecked(config.Db_name, chanid)
-	treeWidget.ResizeColumnToContents(0)
-	treeWidget.ResizeColumnToContents(1)
-	treeWidget.ResizeColumnToContents(2)
-	treeWidget.ResizeColumnToContents(3)
-	treeWidget.ResizeColumnToContents(4)
-	treeWidget.ResizeColumnToContents(5)
-	treeWidget.ConnectDoubleClicked(func(index *core.QModelIndex) {
-		data := index.Data(int(core.Qt__UserRole)).ToString()
-		item := treeWidget.CurrentColumn()
 
-		//get item
-		//one item will download, one will view in browser, one will search for similar subjects
-		//https://www.youtube.com/watch?v=
-		if item == 0 || item == 6 {
-			// fmt.Println(data)
-
-			functions.MkCrawljob(config.Db_name, config.FolderWatch, chanid, treeWidget.CurrentItem().Text(1), data, treeWidget.CurrentItem().Text(2), 1)
-			//double click means open the settings for this channel
-			widgets.QMessageBox_Information(nil, "OK", "Added to Queue "+treeWidget.CurrentItem().Text(1), widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
-
-		}
-		if item == 4 {
-			url := YtWatchPrefix + treeWidget.CurrentItem().Text(0)
-			functions.Openbrowser(config.Defbrowser, url)
-		}
-		if item == 5 {
-			if database.GetVideoExist(config.Db_name, treeWidget.CurrentItem().Text(0)) == 0 {
-				//if this is true then it's coming from the feed
-				unixdate := functions.ConvertYMDtoUnix(treeWidget.CurrentItem().Text(2))
-				database.InsertVideo(config.Db_name, treeWidget.CurrentItem().Text(0), treeWidget.CurrentItem().Text(1), treeWidget.CurrentItem().Text(1), chanid, unixdate, "1")
-				widgets.QMessageBox_Information(nil, "OK", treeWidget.CurrentItem().Text(1)+" Recorded as downloaded", widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
-			}
-		}
-		if item == 1 {
-			text := cleanUnicode(treeWidget.CurrentItem().Text(item))
-			//seting sort order by date uploaded
-			/*
-				sp=CAI%253D #search by upload date
-				sp=CAASAhAB #search by  relevance
-				sp=EgQIAhAB #today and relevance
-				sp=CAISBAgCEAE%253D #today & upload date
-				sp=CAMSBAgCEAE%253D #today and view count
-				sp=CAESBAgCEAE%253D #today and rating
-				sp=CAESAhAB #rating only
-			*/
-			url := YtSearchPrefix + text + "&sp=CAI%253D" //order by upload date
-			functions.Openbrowser(config.Defbrowser, url)
-		}
-
-	})
-
-	layout.AddWidget(treeWidget, 0, 0)
-	mainWidget.SetLayout(layout)
+	mainWidget.SetLayout(formlayout)
 	window.SetCentralWidget(mainWidget)
 	window.Show()
 }
